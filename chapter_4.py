c339fdbaaea06d42ca8fe263d2b1aa1dba89208b
Blake French <blakefren@gmail.com>
2019-03-31 13:24:13 -0600

    Create chapter_4.py

---
+++ chapter_4.py
@@ -0,0 +1,111 @@
+"""
+Chapter Four - Trees and Graphs
+"""
+
+
+# 4.1
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_one():
+    pass
+
+
+# 4.2
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_two():
+    pass
+
+
+# 4.3
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_three():
+    pass
+
+
+# 4.4
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_four():
+    pass
+
+
+# 4.5
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_five():
+    pass
+
+
+# 4.6
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_six():
+    pass
+
+
+# 4.7
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_seven():
+    pass
+
+
+# 4.8
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_eight():
+    pass
+
+
+# 4.9
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_nine():
+    pass
+
+
+# 4.10
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_ten():
+    pass
+
+
+# 4.11
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_eleven():
+    pass
+
+
+# 4.12
+# Description
+# 
+# This method has O(____) runtime where N=____.
+
+def four_twelve():
+    pass
